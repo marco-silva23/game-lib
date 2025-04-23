0fa271b7dba19536c6f3793f647246931e30150f
marco-silva23 <silva4marco@gmail.com>
2025-04-23 07:19:01 -0400

    fixing vite tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import path from 'path';
 
-export default defineConfig({
+export default {
   plugins: [],
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
-  base: '/',
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+};
